016a0daeeaa801014c84255c9242123407dc3fd7
Linh Dan Nguyen <dannguyenyi@gmail.com>
2023-01-30 23:07:18 +0700

    Initial commit

---
+++ app/src/main/java/com/example/w01_empty_username/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.w01_empty_username
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
